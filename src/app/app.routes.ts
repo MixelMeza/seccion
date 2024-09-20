20a7297bf3b702a419450ef0eab63f28d3d4e9bd
MixelMeza <mixolms@gmail.com>
2024-09-19 19:15:49 -0500

    Agregar ediciones y cambios al proyecto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { CategoriaFormComponent } from './categoria-form/categoria-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Pagina de inicio'
+  },
+  {
+    path: 'categoria-form/:id',
+    component: CategoriaFormComponent,
+    title: 'Formulario de categoria'
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+];
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import { Categoria } from '../models/categoria';
+import { CategoriaService } from '../services/categoria.service';
+import { TableModule } from 'primeng/table';
+import { Seccion } from '../models/seccion';
+import { SeccionService } from '../services/seccion.service';
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [CardModule, ButtonModule, RouterModule, TableModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  categorias: Categoria[] = [];
+
+  constructor(private categoriaService: CategoriaService,private seccionService: SeccionService) { }
+
+
+  ngOnInit(): void {
+    this.listarCategoria();
+    this.listarSeccion();
+  }
+  listarCategoria() {
+    this.categoriaService.getCategorias().subscribe((data) => {
+      this.categorias = data;
+    });
+  }
+
+  secciones: Seccion[] = [];
+
+  listarSeccion(){
+    this.seccionService.getSecciones().subscribe((data) => {
+      this.secciones = data;
+    });
+  }
+}
+
